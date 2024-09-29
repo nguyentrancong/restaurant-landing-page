238de0244099ee05a97a02a2a5b9659d78e039f3
Nguyen Tran Cong <nguyentrancong>
2024-09-29 20:47:17 +0700

    Restaurant Landing Page Reactjs

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Logo from "../Assets/Logo.svg";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="footer-icons">
+          <BsTwitter />
+          <SiLinkedin />
+          <BsYoutube />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Quality</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>Careers</span>
+          <span>Testimonials</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>098-888-5555</span>
+          <span>support@mail.com</span>
+          <span>contact@mail.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
